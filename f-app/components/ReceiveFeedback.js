9d76069c4ecb6c3008c06ef2c6233b76df1ced1c
Stefan Matic <stefan.matic@me.com>
2017-11-16 18:00:26 +0100

    Move getFeedback

--- f-app/components/ReceiveFeedback.js
+++ f-app/components/ReceiveFeedback.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import { Text, ScrollView, StyleSheet, View } from 'react-native';
 
+const API_ENDPOINT = 'http://localhost:5002/api/';
+
 export class ReceiveFeedback extends React.Component {
+    getFeedback() {
+        fetch(API_ENDPOINT + 'feedback', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          }
+        }).then(function (result) {
+          let _feedbackList = {
+            feedback: [
+              {
+                Id: 1,
+                IsPositive: true,
+                Created: '2017-11-16 11:27'
+              },
+              {
+                id: 2,
+                IsPositive: true,
+                Created: '2017-11-16 11:36'
+              },
+              {
+                id: 3,
+                IsPositive: false,
+                Created: '2017-11-16 12:10'
+              }
+            ]
+          };
+          this.setState({ feedbackList: _feedbackList });
+        });
+      }
   render() {
     //return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />;
     return <ScrollView style={styles.wrapper} contentContainerStyle={styles.contentContainer}>
