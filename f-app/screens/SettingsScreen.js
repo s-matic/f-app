907c46ed0655f5b3af8822462850f3e2ca34b554
Stefan Matic <stefan.matic@me.com>
2017-11-17 10:29:52 +0100

    Update settings screen

--- f-app/screens/SettingsScreen.js
+++ f-app/screens/SettingsScreen.js
@@ -1,14 +1,68 @@
 import React from 'react';
+import { View, StyleSheet, AsyncStorage } from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
+import { Button } from 'react-native-elements'
+
+const API_ENDPOINT = 'http://localhost:5002/api/';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Inställningar',
   };
+  state = {
+    userType: ''
+  };
+  getUserType() {
+    try {
+      const value = await AsyncStorage.getItem('@FappStore:userType');
+      if (value !== null) {
+        // We have data!!
+        console.log(value);
+        this.setState({userType: value})
+      }
+    } catch (error) {
+      // Error retrieving data'
+      console.log(error);
+    }
+  }
 
+  clearFeedback() {
+    fetch(API_ENDPOINT + 'feedback', {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(function () {
+      Alert.alert(
+        'Feedback rensad',
+        'Din feedbacklista har rensats!',
+        [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false }
+      )
+    });
+  }
+
+  componentWillMount(){
+    this.getUserType();
+  }
   render() {
+    console.log(this.state.userType);
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    let feedbackText = this.state.userType == 'sender' ? 'byt till att ta emot feedback' : 'byt till att ge feedback';
+
+    return <View style={styles.contentContainer}> 
+      <Button title='Rensa feedbacklista' onPress={() => { this.clearFeedback() }} />
+
+      <Button title={feedbackText} onPress={() => { this.clearFeedback() }} buttonStyle={{backgroundColor: 'blue'}} />
+    </View>;
   }
-}
+};
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 20
+  },
+});
